1dab3af67ad25fb93326df714f45961479edc2a8
Tor <pierre.aymeric.masse@gmail.com>
2015-10-18 01:07:34 +0200

    change origin CORS for *

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var flash       = require('connect-flash');
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'localhost');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
